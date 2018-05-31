7f63fc33aef093fa4fa9bacec7aa9bb74738e7a9
rpyoungdale <youngdale95@gmail.com>
2018-05-31 16:08:21 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,18 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [] }, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return { friends: [...state.friends, action.friend] };
+    case "REMOVE_FRIEND":
+      const friendIndex = state.friends.findIndex(
+        friend => friend.id === action.id
+      );
+      return {
+        friends: [
+          ...state.friends.slice(0, friendIndex),
+          ...state.friends.slice(friendIndex + 1)
+        ]
+      };
+    default:
+      return state;
+  }
 }
